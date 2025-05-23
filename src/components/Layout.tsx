23d2a247d23081fb3f8fccf9809acc82aa98f0f8
Syedabidrazazaidi <zaidiabid246@gmail.com>
2025-05-23 08:28:04 +0530

    ok

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -142,7 +142,7 @@ export default function Layout() {
                 <div className="mt-4 space-y-2">
                   <div className="flex items-center space-x-2 text-gray-400">
                     <Mail className="w-5 h-5" />
-                    <span>support@proservices.com</span>
+                    <span>support@getproworker.com</span>
                   </div>
                   <div className="flex items-center space-x-2 text-gray-400">
                     <Phone className="w-5 h-5" />
